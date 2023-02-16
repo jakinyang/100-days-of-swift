c72b913b27caa430005730bd7cd43e8c5776b42d
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-15 22:37:05 -0800

    --finish day3 lesson 1

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -3,3 +3,32 @@ import Cocoa
 var beetles = ["John", "Paul", "George", "Ringo"]
 let numbers = [1, 2, 3, 4, 5, 6]
 var temperatures = [1.1, 2.3, 4.5]
+
+beetles.append("Adrian")
+
+var scores = Array<Int>()
+scores.append(100)
+scores.append(20)
+scores.append(39)
+
+scores
+
+var albums = Array<String>()
+
+albums.append("Folklore")
+albums.append("Fearless")
+albums.append("1989")
+
+print(albums.count)
+
+albums.remove(at: 2)
+
+print(albums)
+
+print(albums.contains("Fearless"))
+
+numbers.reversed()
+numbers
+
+scores.sort()
+
