5362aa2a75cdc8adf2315ebcc30301009afbd24e
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-15 23:19:52 -0800

    --finish day 3

--- Day3.playground/Contents.swift
+++ Day3.playground/Contents.swift
@@ -1,5 +1,6 @@
 import Cocoa
 
+// Arrays
 var beetles = ["John", "Paul", "George", "Ringo"]
 let numbers = [1, 2, 3, 4, 5, 6]
 var temperatures = [1.1, 2.3, 4.5]
@@ -32,3 +33,40 @@ numbers
 
 scores.sort()
 
+// Dictionaries
+let singer = [
+    "name": "Taylor Swift",
+    "job": "Singer",
+    "location": "Nashville"
+]
+
+print(singer["name", default: "Unknown"])
+print(singer["job", default: "Unknown"])
+print(singer["location", default: "Unknown"])
+print(singer["manager", default: "Unknown"])
+
+var heights = [String: Int]()
+heights["Jae"] = 178
+
+// Set
+let people = Set(["John", "Paul", "George", "Ringo", "Phil"])
+
+var raccoons = Set<String>()
+raccoons.insert("Joey")
+raccoons.insert("Rumpus")
+raccoons.insert("Joey")
+raccoons.insert("Carl")
+raccoons.insert("Edgar")
+print(raccoons)
+
+// Enums
+
+enum Weekday {
+    case Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+}
+
+var day = Weekday.Monday
+day = .Friday
+day = .Tuesday
+
+print("It's \(day), \(day), gotta get down on \(day)")
