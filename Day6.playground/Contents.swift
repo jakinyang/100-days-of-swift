74756f15efac7ebf52d1bb95beb6418ae7e4a35b
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-17 11:22:10 -0800

    --add FizzBuzz

--- Day6.playground/Contents.swift
+++ Day6.playground/Contents.swift
@@ -1,15 +1,27 @@
 import Cocoa
 
+// For Loop
+
 let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
 
 for os in platforms {
     print("Swift works great on \(os)")
 }
 
-for i in 1...20 {
+for i in 1..<21 {
     if (i == 1) {
         print("There is 1 raccoon on the deck!")
     } else {
         print("There are \(i) raccoons on the deck!")
     }
 }
+
+// While Loop
+
+var countdown = 10
+while countdown > 0 {
+    print(countdown)
+    countdown -= 1
+}
+
+print("Blast off!")
