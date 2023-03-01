5a5d46913b0dfc8f8778e64cef0b05138f9092e4
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-28 20:03:21 -0800

    --complete checkpoint 5

--- Day9.playground/Contents.swift
+++ Day9.playground/Contents.swift
@@ -140,4 +140,12 @@ doImportantWork {
     print("This is the third work!")
 }
 
+let luckyNumbers = [7, 4, 38, 21, 16, 15, 12, 33, 31, 49]
 
+luckyNumbers.filter {
+    !$0.isMultiple(of: 2)
+}.sorted {
+    return $0 < $1
+}.map {
+    print("\($0) is a lucky number")
+}
